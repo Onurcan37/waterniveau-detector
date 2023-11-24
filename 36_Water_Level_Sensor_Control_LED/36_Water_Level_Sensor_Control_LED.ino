fe7118501e8c88b0eea7b1ef123f70a6352da7c4
Onurcan37 <gozuacik3737@hotmail.com>
2023-11-24 11:00:52 +0100

    Nieuwe Waterniveau script.
    Script is compleet aangepast, door een nieuwe waterlevel sensor.

--- 36_Water_Level_Sensor_Control_LED/36_Water_Level_Sensor_Control_LED.ino
+++ 36_Water_Level_Sensor_Control_LED/36_Water_Level_Sensor_Control_LED.ino
@@ -1,60 +1,140 @@
+#include <Wire.h>
 #include <Adafruit_ST7735.h>
 #include <Adafruit_ST7789.h>
 #include <Adafruit_ST77xx.h>
+
+#define SERIAL Serial
 
 #define TFT_CS        10
 #define TFT_RST        9 // Or set to -1 and connect to Arduino RESET pin
 #define TFT_DC         8
 
+unsigned char low_data[8] = {0};
+unsigned char high_data[12] = {0};
+
+#define NO_TOUCH       0xFE
+#define THRESHOLD      100
+#define ATTINY1_HIGH_ADDR   0x78
+#define ATTINY2_LOW_ADDR   0x77
+
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
-int sensorPin = A0; 
-int sensorValue = 0;
-
-void setup()
+void getHigh12SectionValue(void)
 {
-  Serial.begin(9600);
-  Serial.print(F("Hello! ST77xx TFT Test"));
-  // Use this initializer if using a 1.8" TFT screen:
-  tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
+  memset(high_data, 0, sizeof(high_data));
+  Wire.requestFrom(ATTINY1_HIGH_ADDR, 12);
+  while (12 != Wire.available());
 
+  for (int i = 0; i < 12; i++) {
+    high_data[i] = Wire.read();
+  }
+  delay(10);
+}
+
+void getLow8SectionValue(void)
+{
+  memset(low_data, 0, sizeof(low_data));
+  Wire.requestFrom(ATTINY2_LOW_ADDR, 8);
+  while (8 != Wire.available());
+
+  for (int i = 0; i < 8 ; i++) {
+    low_data[i] = Wire.read(); // receive a byte as character
+  }
+  delay(10);
+}
+
+void check()
+{
+  int sensorvalue_min = 250;
+  int sensorvalue_max = 255;
+  int low_count = 0;
+  int high_count = 0;
+  while (1)
+  {
+    uint32_t touch_val = 0;
+    uint8_t trig_section = 0;
+    low_count = 0;
+    high_count = 0;
+    getLow8SectionValue();
+    getHigh12SectionValue();
+
+    Serial.println("low 8 sections value = ");
+    for (int i = 0; i < 8; i++)
+    {
+      Serial.print(low_data[i]);
+      Serial.print(".");
+      if (low_data[i] >= sensorvalue_min && low_data[i] <= sensorvalue_max)
+      {
+        low_count++;
+      }
+      if (low_count == 8)
+      {
+        Serial.print("      ");
+        Serial.print("PASS");
+      }
+    }
+    Serial.println("  ");
+    Serial.println("  ");
+    Serial.println("high 12 sections value = ");
+    for (int i = 0; i < 12; i++)
+    {
+      Serial.print(high_data[i]);
+      Serial.print(".");
+
+      if (high_data[i] >= sensorvalue_min && high_data[i] <= sensorvalue_max)
+      {
+        high_count++;
+      }
+      if (high_count == 12)
+      {
+        Serial.print("      ");
+        Serial.print("PASS");
+      }
+    }
+
+    Serial.println("  ");
+    Serial.println("  ");
+
+    for (int i = 0 ; i < 8; i++) {
+      if (low_data[i] > THRESHOLD) {
+        touch_val |= 1 << i;
+
+      }
+    }
+    for (int i = 0 ; i < 12; i++) {
+      if (high_data[i] > THRESHOLD) {
+        touch_val |= (uint32_t)1 << (8 + i);
+      }
+    }
+
+    while (touch_val & 0x01)
+    {
+      trig_section++;
+      touch_val >>= 1;
+    }
+
+    tft.fillScreen(ST77XX_BLACK);
+    tft.setCursor(0, 0);
+    tft.println("Waterniveau:");
+
+    tft.print(trig_section * 5);
+    tft.println(" millimeter");
+
+    delay(1000);
+  }
+}
+
+void setup() {
+  SERIAL.begin(115200);
+  Wire.begin();
+
+  tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
   tft.setRotation(-45);
   tft.setTextColor(ST77XX_WHITE);
   tft.setTextSize(2);
 }
 
-void loop() 
+void loop()
 {
-  sensorValue = analogRead(sensorPin);
-  tft.fillScreen(ST77XX_BLACK);
-
-  tft.setCursor(0, 0);
-  tft.println("Waterniveau");
-  tft.println(sensorValue);
-
-  Serial.println(sensorValue);
-
-  if (sensorValue > 670)
-  {
-    tft.println("Zeer hoog");
-  }
-  else if (sensorValue > 664) 
-  {
-    tft.println("Hoog");
-  }
-  else if (sensorValue > 660)
-  {
-  tft.println("Gemiddeld");
-  }
-  else if (sensorValue > 650)
-  {
-  tft.println("Laag");
-  }
-  else
-  {
-  tft.println("Droog");
-  }
-  Serial.println(sensorValue);
-
-  delay(100);
+  check();
 }
